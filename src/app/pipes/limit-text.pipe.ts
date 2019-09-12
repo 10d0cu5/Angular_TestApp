8ba3a6394351487bda0de2cd1c3907809ede4d39
Jonas Leeb <mobilecollectspam@gmail.com>
2019-09-13 00:53:10 +0200

    design polish

--- src/app/pipes/limit-text.pipe.ts
+++ src/app/pipes/limit-text.pipe.ts
@@ -3,6 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'cut_text'})
 export class CutTextPipe implements PipeTransform{
     transform(text: string): string{
-        return text.substr(0,10);
+        if(text.length > 10){
+            return text.substr(0,10) + '...';
+        }else{
+            return text;
+        }
+        
     }
 }
